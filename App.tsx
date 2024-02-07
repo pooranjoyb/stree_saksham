261f23d9f2a39bb8b4a2511199abf761879deda8
pooranjoyb <pooranjoyb2016@gmail.com>
2024-02-08 00:36:52 +0530

    Added Landing PAge

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Landing from './pages/Landing';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Landing />
       <StatusBar style="auto" />
     </View>
   );
---
+++ pages/Landing.tsx
@@ -0,0 +1,104 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import { 
+    View,
+    Text, 
+    StyleSheet, 
+    Image,
+    Pressable
+} from 'react-native'
+import React from 'react'
+
+const Landing = () => {
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Stree-Saksham Welcomes You!
+            </Text>
+            <LinearGradient colors={['#FF4D6D', '#FF8FA3', '#FFB3C1']} style={styles.container2}>
+                <Image source={require('../assets/images/woman.png')}
+                    style={styles.image} />
+
+                    <Pressable style={styles.button1}>
+                        <Text style={styles.btnText}>
+                            Get Started
+                        </Text>
+                    </Pressable>
+                    <Pressable style={styles.button2}>
+                        <Text style={styles.btnText}>
+                            Login
+                        </Text>
+                    </Pressable>
+            </LinearGradient>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: "#FFB3C1",
+        justifyContent: "center",
+
+    },
+    title: {
+        display: 'flex',
+        alignItems: 'center',
+        textAlign: 'center',
+        justifyContent: 'center',
+        fontSize: 42,
+        padding: 60,
+    },
+    container2: {
+        height: '70%',
+        width: '100%',
+        borderRadius: 50
+    },
+
+    image: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '40%',
+        width: '90%',
+        marginTop: 25,
+        borderRadius: 50,
+        marginLeft: 20,
+    },
+    button1:{
+        position: 'absolute',
+        bottom: 160,
+        left: 80,
+        display: 'flex',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#D9D9D9',
+        height: 66,
+        width: 239,
+        borderRadius: 50
+    },
+    button2:{
+        position: 'absolute',
+        bottom: 70,
+        left: 80,
+        display: 'flex',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FF758F',
+        height: 66,
+        width: 239,
+        borderRadius: 50
+    },
+    btnText:{
+        display: 'flex',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 32,
+        color: '#590D22'
+    }
+})
+
+export default Landing
